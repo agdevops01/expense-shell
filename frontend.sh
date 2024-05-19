483dadb49d3183f3cf8e5026a383dee21160329d
ankitguptarbr <ankitguptarbr.gupta@gmail.com>
2024-05-19 09:55:03 +0100

    frontend echo heading expense-shell update

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,21 @@
-HEADING Installing Nginx
+echo HEADING Installing Nginx
 dnf install nginx -y
 
-HEADING Copy expense config file
+echo HEADING Copy expense config file
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-HEADING clean old content
+echo HEADING clean old content
 rm -rf /usr/share/nginx/html/*
 
-HEADING download frontend content
+echo HEADING download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 
 cd /usr/share/nginx/html
 
-HEADING extract content
+echo HEADING extract content
 unzip /tmp/frontend.zip
 
-HEADING restart servic
+echo HEADING restart servic
 systemctl restart nginx
 systemctl enable nginx
 
